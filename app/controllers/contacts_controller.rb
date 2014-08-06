8721b245748d072d290472f333fce74eb1f512fe
Aman Jain <amankj@Amans-MBP-wSSD.local>
2014-08-05 23:11:04 -0500

    Working email and notices

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,10 +7,12 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      # render template: "home/index"
+      redirect_to root_url, notice: "Thank you for your message. We will contact you soon!"
+
     else
       flash.now[:error] = 'Cannot send message.'
-      render :new
+      render :nothing => true
     end
   end
 end
