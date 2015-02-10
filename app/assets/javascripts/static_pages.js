885dd166cbd1c2f007b1ba6e5d114d5963cf5917
Aman Jain <amankj@gmail.com>
2015-02-09 23:10:06 -0600

    working tabs across all

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -16,3 +16,40 @@ $(document).ready(function($) {
   $(".linkbar>ul>li:eq(0)>a").click();
 
 });
+
+// $(document).ready(function($) {
+//   function changeInitiativeTab(e) {
+//     e.preventDefault();
+//     $(".linkbar>ul>li>a.active").removeClass("active");
+//     $(this).addClass("active");
+//     showInitiativeTab($(this).attr("href"));
+
+//   }
+
+//   function showInitiativeTab(activeDiv) {
+//     $("#main-content>div").hide();
+//     $(activeDiv).show();
+//   }
+
+//   $(".footer-2015>a").click(changeInitiativeTab);
+
+// });
+
+$(document).ready(function($) {
+  function changeInstallerTab(e) {
+    e.preventDefault();
+    $(".installers>ul>li>a.active").removeClass("active");
+    $(this).addClass("active");
+    showInstallerTab($(this).attr("href"));
+
+  }
+
+  function showInstallerTab(activeDiv) {
+    $(".installer-details>div").hide();
+    $(activeDiv).show();
+  }
+
+  $(".installers>ul>li>a").click(changeInstallerTab);
+  $(".installers>ul>li:eq(0)>a").click();
+
+});
